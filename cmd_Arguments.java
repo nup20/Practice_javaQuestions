282d5444c1eaf18f2fd4aef810a4b58ac78b4ae9
nup20 <nupurthakre16@gmail.com>
2023-10-28 20:01:34 +0530

    cmd

---
+++ cmd_Arguments.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class cmd_Arguments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(String a:args){
+	System.out.println(a);
+}
+	}
+
+}
