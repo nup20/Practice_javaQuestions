0f2365ed2d0984c5ece19c47fada0833260c9877
nup20 <nupurthakre16@gmail.com>
2023-11-04 20:08:59 +0530

    commit

---
+++ Scanner_input.java
@@ -0,0 +1,38 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Scanner_input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter String:");
+		String a=sc.next();
+		System.out.println(a);
+		
+		System.out.println("enter double:");
+		double b=sc.nextDouble();
+		System.out.println(b);
+
+		
+		System.out.println("enter int:");
+		int c=sc.nextInt();
+		System.out.println(c);
+
+		
+		System.out.println("enter Float:");
+		float f=sc.nextFloat();
+		System.out.println(f);
+
+		
+		System.out.println("enter char:");
+		String g=sc.next();
+		System.out.println(g);
+
+		
+		
+	}
+
+}
