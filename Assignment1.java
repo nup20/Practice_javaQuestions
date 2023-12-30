18b3a562b87318afa8241f418b253b70113d5d31
nup20 <nupurthakre16@gmail.com>
2023-12-30 23:04:06 +0530

    .

---
+++ Assignment1.java
@@ -0,0 +1,29 @@
+package Assignments;
+import java.util.Scanner;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a;
+ float b;
+ String c;
+ double d;
+ 
+ Scanner sc=new Scanner(System.in);
+ a=sc.nextInt();
+ System.out.println("Enter integer:");
+ 
+ b=sc.nextFloat();
+ System.out.println("Enter integer:");
+ 
+ c=sc.String();
+ System.out.println("Enter integer:");
+ 
+ 
+ 
+ 
+ 
+	}
+
+}
