708bb5d977a34ca769c6b6fdb4e3f5dd9e9de77e
nup20 <nupurthakre16@gmail.com>
2024-01-03 20:54:53 +0530

    .

--- Assignment 2.java
+++ Assignment 2.java
@@ -1,29 +1,31 @@
-package Assignments;
-import java.util.Scanner;
-
-public class Assignment1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- int a;
- float b;
- String c;
- double d;
+public class Exercise6 {
  
- Scanner sc=new Scanner(System.in);
- a=sc.nextInt();
- System.out.println("Enter integer:");
+ public static void main(String[] args) {
+  // Create a Scanner object to read input from the user
+  Scanner in = new Scanner(System.in);
+   
+  // Prompt the user to input the first number
+  System.out.print("Input first number: ");
+  // Read and store the first number
+  int num1 = in.nextInt();
+   
+  // Prompt the user to input the second number
+  System.out.print("Input second number: ");
+  // Read and store the second number
+   
+  // Calculate and print the sum of the two numbers
+  System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+   
+  // Calculate and print the difference of the two numbers
+  System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+   
+  // Calculate and print the product of the two numbers
+  System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+   
+  // Calculate and print the division of the two numbers
+  System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
  
- b=sc.nextFloat();
- System.out.println("Enter integer:");
- 
- c=sc.String();
- System.out.println("Enter integer:");
- 
- 
- 
- 
- 
-	}
-
-}
+  // Calculate and print the remainder of the division of the two numbers
+  System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2);
+ }
+} 
