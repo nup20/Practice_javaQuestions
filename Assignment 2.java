002560f7c0d7b7b0fb1e87e98aba2afc93339f6d
nup20 <nupurthakre16@gmail.com>
2024-01-04 21:29:37 +0530

    .

--- Assignment 2.java
+++ Assignment 2.java
@@ -5,27 +5,27 @@ public class Exercise6 {
   Scanner in = new Scanner(System.in);
    
   // Prompt the user to input the first number
-  System.out.print("Input first number: ");
-  // Read and store the first number
-  int num1 = in.nextInt();
-   
-  // Prompt the user to input the second number
-  System.out.print("Input second number: ");
-  // Read and store the second number
-   
-  // Calculate and print the sum of the two numbers
-  System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-   
-  // Calculate and print the difference of the two numbers
-  System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-   
-  // Calculate and print the product of the two numbers
-  System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
-   
-  // Calculate and print the division of the two numbers
-  System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
- 
-  // Calculate and print the remainder of the division of the two numbers
-  System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2);
- }
-} 
+  System.out.print("Input first number: ");package TCQ;
+
+import java.util.Scanner;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the 1st number=");
+int a=sc.nextInt();
+System.out.print("Enter the 2st number=");
+int b=sc.nextInt();
+
+System.out.println(a+"+"+b+"="+(a+b));
+System.out.println(a+"-"+b+"="+(a-b));
+System.out.println(a+"*"+b+"="+(a*b));
+System.out.println(a+"/"+b+"="+(a/b));
+
+	}
+
+}
+
+  
